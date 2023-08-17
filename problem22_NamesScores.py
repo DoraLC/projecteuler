e0f2395956e085760ded7969ce44dba4a277733e
DoraLC <34760377+DoraLC@users.noreply.github.com>
2023-08-17 14:19:12 +0800

    updates

--- problem22_NamesScores.py
+++ problem22_NamesScores.py
@@ -8,4 +8,31 @@ For example, when the list is sorted into aplhabetical order, COLIN, which is wo
 is the 938th name in the list. So, COLIN would obtain a score of 938 x 53 = 49714. 
 
 What is the total of all the name scores in the file?
+answer: 871198282
 '''
+import string
+
+names = open('0022_names.txt', 'r')
+alphabit = list(string.ascii_uppercase)
+letter_score = {}
+for i in range(0, len(alphabit)):
+    letter_score[alphabit[i]] = i + 1
+letter_score['"'] = 0
+
+def worth(name):
+    rtn = 0
+    for letter in name:
+        rtn += letter_score[letter]
+    return rtn
+
+names_list = names.read().split(',')
+names.close()
+names_list.sort()
+
+def solution(names):
+    rtn = 0
+    for i in range(len(names)):
+        rtn += worth(names[i]) * (i + 1)
+    return rtn
+
+print(solution(names_list))
